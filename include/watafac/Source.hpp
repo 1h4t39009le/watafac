11ad21400874defb09846add9d79c9f5f61f41da
1h4t39009le <1h4t39009le@gmail.com>
2026-04-22 11:21:48 +0300

    Added source lib to read source code in lexer parser etc

---
+++ include/watafac/Source.hpp
@@ -0,0 +1,21 @@
+#pragma once
+#include <string>
+#include <memory>
+#include <optional>
+namespace wfac {
+    class Source {
+    public:
+        virtual std::unique_ptr<std::istream> get_istream() = 0;
+        virtual ~Source() = default;
+    };
+    class FileSource: public Source{
+    public:
+        explicit FileSource(std::string filepath);
+        std::unique_ptr<std::istream> get_istream() override;
+    private:
+        static constexpr std::size_t CONTENT_THRESHOLD = 1024 * 1024; //1MB
+        void load_content();
+        std::string filepath_;
+        std::optional<std::string> content_;
+    };
+}
--- tools/wfac/wfac.cpp
+++ tools/wfac/wfac.cpp
@@ -1,29 +1,12 @@
 #include <iostream>
 #include <memory>
 #include <fstream>
-namespace wfac {
-    class Source {
-    public:
-        virtual std::unique_ptr<std::istream> get_istream() = 0;
-        virtual ~Source() = default;
-    };
-    class FileSource: public Source{
-    public:
-        explicit FileSource(std::string filepath)
-        : filepath_{std::move(filepath)} {}
-        
-        std::unique_ptr<std::istream> get_istream() override {
-            auto stream = std::make_unique<std::ifstream>(filepath_);
-            if(!stream->is_open()) return nullptr;
-            return stream;
-        }
-    private:
-        std::string filepath_;
-    };
-}
+#include <watafac/Source.hpp>
 
 int main(){
-    auto src = std::make_shared<wfac::FileSource>("a.txt");
-    
+    auto shrsrc = std::make_shared<wfac::FileSource>("a.txt");
+    if(auto stream = shrsrc->get_istream()){
+        std::cout << "File found!\n";
+    }
     std::cout << "Hello, World!\n";
 }
